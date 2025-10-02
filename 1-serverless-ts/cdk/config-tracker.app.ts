8d7bc7cd2263c8aedf1ebbc40aa707b336596d7a
Michal Moravčík <miso.moravcik@gmail.com>
2025-10-02 05:49:13 +0200

    1-serverless-ts - added log retention (1 week) for lambda functions

--- 1-serverless-ts/cdk/config-tracker.app.ts
+++ 1-serverless-ts/cdk/config-tracker.app.ts
@@ -2,15 +2,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Duration } from 'aws-cdk-lib';
 import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { ConfigTrackerStack } from './config-tracker.stack';
 
 export const resourcePrefix = 'config-tracker-1-serverless-ts';
 
-export const baseLambdaProps = {
+export const baseLambdaProps: NodejsFunctionProps = {
   runtime: Runtime.NODEJS_22_X,
   architecture: Architecture.ARM_64,
   memorySize: 512,
   timeout: Duration.seconds(10),
+  logRetention: RetentionDays.ONE_WEEK,
   bundling: { minify: true, sourceMap: true, externalModules: [
     '@aws-sdk/client-dynamodb',
     '@aws-sdk/client-sns',
